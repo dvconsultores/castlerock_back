4e61dc23132658c8b7237b0467e018f9ceb4d00e
iOchando <juanochando00@gmail.com>
2025-07-22 13:06:37 +0200

    up

--- src/modules/student/services/student.service.ts
+++ src/modules/student/services/student.service.ts
@@ -116,7 +116,17 @@ export class StudentService {
       .leftJoinAndSelect('student.contacts', 'contacts')
       .leftJoinAndSelect('student.additionalPrograms', 'additionalPrograms')
       .leftJoinAndSelect('student.classes', 'classes')
-      .select(['student', 'campus.id', 'campus.name', 'contacts', 'additionalPrograms', 'classes']);
+      .leftJoinAndSelect('classes.campus', 'classCampus')
+      .select([
+        'student',
+        'campus.id',
+        'campus.name',
+        'contacts',
+        'additionalPrograms',
+        'classes',
+        'classCampus.id',
+        'classCampus.name',
+      ]);
 
     if (query.campusId) {
       queryBuilder.where('campus.id = :campusId', { campusId: query.campusId });
@@ -154,8 +164,7 @@ export class StudentService {
       .leftJoinAndSelect('student.contacts', 'contacts')
       .leftJoinAndSelect('student.additionalPrograms', 'additionalPrograms')
       .leftJoinAndSelect('student.classes', 'classes')
-      .leftJoinAndSelect('student.classes.campus', 'campusClasses')
-      .select(['student', 'campus.id', 'campus.name', 'contacts', 'additionalPrograms', 'classes', 'classes.campus'])
+      .select(['student', 'campus.id', 'campus.name', 'contacts', 'additionalPrograms', 'classes'])
       .where('student.id = :id', { id })
       .getOne();
 
